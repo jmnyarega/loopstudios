7b707eedc842dc0bb2bbdffa9fd87c7299a669f5
josiahmokob0 <jmnyarega@gmail.com>
2021-09-01 12:30:33 +0300

    - cleaning ...

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setTemplateFormats(["css", "svg", "md", "jpg", "njk"]);
+  eleventyConfig.setTemplateFormats(["css", "svg", "md", "jpg", "njk", "png"]);
   return {
     dir: {
       input: "src",
